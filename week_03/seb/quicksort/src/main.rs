3a5bfd50095edf964984daf9f995baa3820f59f5
Sebastian Axelsen <sa@palmtreestatistics.com>
2017-05-08 22:32:10 +0200

    Adds quicksort algorithm for a vector of ints

--- week_03/seb/quicksort/src/main.rs
+++ week_03/seb/quicksort/src/main.rs
@@ -1,23 +1,68 @@
 use std::vec::Vec;
 
-fn quicksort_int(list: &Vec<i32>) -> Vec<i32> {
+fn quicksort_int(list: &Vec<i32>, ascending: bool) -> Vec<i32> {
 
     let mut newlist = list.clone();
-    newlist.swap(1,2);
+    let mut partitions = Vec::new();
+    partitions.push((0, list.len()));
 
-    for i in &newlist {
-        println!("{}", i);
+    let mut partition;
+    let mut partition_start;
+    let mut partition_end;
+
+    while partitions.len() > 0 {
+        partition = partitions.pop();
+        match partition {
+            Some(x) => {
+                partition_start = x.0;
+                partition_end = x.1;
+            },
+            None => {
+                panic!("Cannot pop from empty list.")
+            },
+        }
+        let pivot_index = partition_start;
+        let pivot_value = newlist[pivot_index];
+        let mut store_index = pivot_index + 1;
+
+        for i in pivot_index + 1..partition_end {
+            let current_value = newlist[i];
+            
+            let comparison = if ascending {
+                current_value < pivot_value
+            }   else {
+                current_value > pivot_value
+            };
+
+            if comparison {
+                newlist.swap(i, store_index);
+                store_index += 1;
+            }
+        }
+        newlist.swap(pivot_index, store_index - 1);
+
+        if partition_start != store_index - 1 {
+            partitions.push((partition_start, store_index - 1));
+        }
+
+        if partition_end != store_index {
+            partitions.push((store_index, partition_end))
+        }
+
     }
 
     newlist
 }
 
 fn main() {
-    let list = vec![1,2,3,4];
-    println!("New list:");
-    quicksort_int(&list);
-    println!("Old list:");
+    let list = vec![5,1,-5,2,30,10,8];
+    println!("List:");
     for i in &list {
+        println!("{}", i);
+    }
+    let sorted = quicksort_int(&list, true);
+    println!("Sorted list:");
+    for i in &sorted {
         println!("{}", i);
     }
 }
